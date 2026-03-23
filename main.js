b3dc63419e5244c79d3f3853579c297f2a98c988
TheHCJ <260060686+TheHCJ@users.noreply.github.com>
2026-03-23 22:26:35 +0000

    make footer sticky and adjust fonts

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ checkbox.addEventListener("change", () => {
   if (checkbox.checked) {
     // Change CSS variables when checked
     document.documentElement.style.setProperty("--bg", "#121212");
+    document.documentElement.style.setProperty("--fg", "DimGray");
     document.documentElement.style.setProperty("--text", "#f0f0f0");
     document.documentElement.style.setProperty("--link", "dodgerblue");
   } else {
     // Reset CSS variables when unchecked
     document.documentElement.style.setProperty("--bg", "white");
+    document.documentElement.style.setProperty("--fg", "gainsboro");
     document.documentElement.style.setProperty("--text", "black");
     document.documentElement.style.setProperty("--link", "#008B8B");
   }
