b1e8c5ce464c7c48013d5a0e7eadc27b097a4b0c
TheHCJ <260060686+TheHCJ@users.noreply.github.com>
2026-03-23 22:01:19 +0000

    change link colour based on theme

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ checkbox.addEventListener("change", () => {
     // Change CSS variables when checked
     document.documentElement.style.setProperty("--bg", "#121212");
     document.documentElement.style.setProperty("--text", "#f0f0f0");
+    document.documentElement.style.setProperty("--link", "dodgerblue");
   } else {
     // Reset CSS variables when unchecked
     document.documentElement.style.setProperty("--bg", "white");
     document.documentElement.style.setProperty("--text", "black");
+    document.documentElement.style.setProperty("--link", "#008B8B");
   }
 });
